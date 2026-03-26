89d1a3ed0fb000f7110a9429adf4e90aa7a1abed
Maria Fernanda Manrique M <mafe.manrique.moreno@gmail.com>
2026-03-26 09:16:25 -0500

    feat: configurar conexion a base de datos PostgreSQL y agregar sqlalchemy

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,1 +1,19 @@
-"""Configuración de la conexión a PostgreSQL."""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+# 1. Cargar los secretos de tu archivo .env
+load_dotenv()
+
+# 2. Obtener la URL de conexión que guardaste
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 3. Crear el "Motor". Es el encargado de establecer la conexión real con PostgreSQL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# 4. Crear la fábrica de Sesiones. Cada vez que hagamos una consulta, abriremos una "sesión"
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 5. La Clase Base. De aquí heredarán todas tus tablas (modelos)
+Base = declarative_base()
